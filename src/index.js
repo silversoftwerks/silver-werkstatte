557fdb8946f425999dece570bf5f5c82c4e57f00
Silver Softwerks <engineering@silversoftwerks.com>
2019-06-29 23:32:13 -0600

    Booking Calendar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import ReactDOM from "react-dom";
 import Grid from "@SS/design-system/src/components/layout/Grid.jsx";
 import Column from "@SS/design-system/src/components/layout/Column.jsx";
@@ -10,37 +10,205 @@ import { Header } from "@SS/design-system/src/components/typography/Header";
 import { TextInput } from "@SS/design-system/src/components/form/fillables/TextInput";
 import { Button } from "@SS/design-system/src/components/interactives/Button";
 import { store } from "./models/store";
-const App = () => (
-  <Page>
-    <Billboard height={"60%"}>
-      <Header>Silver Werkstatte</Header>
-      <Header>State of the Art</Header>
-      <Header>Design and Engineering Co-working/Co-living</Header>
-      <Header>Breckenridge Colorado</Header>
-    </Billboard>
-    <Box
-      height={"10%"}
-      justifyContent="center"
-      alignItems="center"
-      backgroundColor="gold"
-    >
-      <TextInput />
-      <Button>Search</Button>
-    </Box>
-    {JSON.stringify(store, null, 2)}
-    <Grid columns={2}>
-      <Column>
-        <Box justifyContent="center" alignItems="center">
-          <Text>Hello World</Text>
+import Logo from "./../../website/src/components/Logo";
+const calendarSelector = store => store;
+import BillboardText from "./../../../design-system/src/components/typography/Billboard";
+const colors = {
+  blue: "#0F4D73",
+  lightBlue: "#1B758D",
+  yellow: "#FCE4BE",
+  orange: "#F3B18C",
+  pink: "#DB888A"
+};
+const App = () => {
+  // const { calendar } = useMemo(calendarSelector(store), [
+  //   calendarSelector(store)
+  // ]);
+  const defaultDateIndex = 0;
+  const defaultState = {
+    ...store,
+    navigation: {
+      dateIndex: defaultDateIndex
+    },
+    authentication: {
+      currentUser: 1
+    },
+    pendingBooking: {
+      startDate: store.dates[defaultDateIndex].id,
+      endDate: store.dates[defaultDateIndex + 1].id
+    },
+    control: { selectStartDate: true }
+  };
+  const [state, setState] = useState(defaultState);
+  const {
+    debug = true,
+    dates,
+    holidays,
+    buildings,
+    rooms,
+    bookings,
+    users,
+    pendingBooking,
+    navigation: { dateIndex },
+    control: { selectStartDate },
+    authentication: { currentUser }
+  } = state;
+  const [mouseDown, setMouseDown] = useState(false);
+
+  return (
+    <Page>
+      {/* <Box>
+      {debug && JSON.stringify(store, null, 2)}
+      </Box> */}
+      <Box>
+        <Billboard
+          padding="96px"
+          backgroundImage='url("https://images.pexels.com/photos/691668/pexels-photo-691668.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")'
+        >
+          <Box height="45%" fillHorizontal={false} flexDirection="column">
+            <Box
+              width="unset"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              {"SILVER".split("").map(char => (
+                <BillboardText>{char}</BillboardText>
+              ))}
+            </Box>
+            <Logo color="white" accentColor={colors.blue} />
+
+            <BillboardText color="white">WERKSTATTE</BillboardText>
+          </Box>
+          {/* <Text color="white">
+          Design and Engineering Co-working/Co-living
+        </Text>
+        <Text color="white">Breckenridge Colorado</Text> */}
+        </Billboard>
+      </Box>
+      <Box flexDirection="column" paddingLeft="10%" paddingRight="10%">
+        <Box paddingVertical="16px" justifyContent="center" alignItems="center">
+          <Box paddingRight="16px" justifyContent="center" alignItems="center">
+            <TextInput
+              width="100%"
+              color={colors.blue}
+              border={`2px solid ${colors.pink}`}
+              outline="none"
+              paddingRight="16px"
+            />
+          </Box>
+          <Button
+            backgroundColor={mouseDown ? "white" : colors.pink}
+            border={`2px solid ${colors.pink}`}
+            outline="none"
+            color={mouseDown ? colors.pink : "white"}
+            onMouseDown={() => setMouseDown(state => true)}
+            onMouseUp={() => setMouseDown(state => false)}
+          >
+            <Text>Search</Text>
+          </Button>
         </Box>
-      </Column>
-      <Column>
-        <Box justifyContent="center" alignItems="center">
-          <Text>GoodBye World</Text>
+        <Box paddingTop="16px" justifyContent="center" alignItems="center">
+          <Header>Book your next adventure</Header>
         </Box>
-      </Column>
-    </Grid>
-  </Page>
-);
+        <Box paddingVertical="16px">
+          <Grid columns={7} grid-gap="16px">
+            {dates
+              .slice(dateIndex, dateIndex + 14)
+              .map(({ label, id, ...rest }) => (
+                <Column>
+                  <Box
+                    backgroundColor={
+                      [
+                        pendingBooking.startDate,
+                        pendingBooking.endDate
+                      ].includes(id)
+                        ? colors.blue
+                        : "white"
+                    }
+                    border={`1px solid ${colors.blue}`}
+                    borderBottom={
+                      pendingBooking.startDate < id &&
+                      id <= pendingBooking.endDate
+                        ? `16px solid ${colors.blue}`
+                        : `1px solid ${colors.blue}`
+                    }
+                    justifyContent="center"
+                    alignItems="center"
+                    height="100px"
+                    textAlign="center"
+                    overflow="hidden"
+                    paddingHorizontal="8px"
+                    onClick={() =>
+                      setState(state => ({
+                        ...state,
+                        control: {
+                          ...state.control,
+                          selectStartDate: !selectStartDate
+                        },
+                        pendingBooking: {
+                          ...state.pendingBooking,
+                          startDate:
+                            selectStartDate &&
+                            state.pendingBooking.startDate != id
+                              ? id
+                              : state.pendingBooking.startDate,
+                          endDate:
+                            !selectStartDate &&
+                            state.pendingBooking.enddate != id
+                              ? Math.max(id, state.pendingBooking.startDate) ==
+                                state.pendingBooking.startDate
+                                ? state.pendingBooking.startDate + 1
+                                : Math.max(id, state.pendingBooking.startDate)
+                              : state.pendingBooking.endDate
+                        }
+                      }))
+                    }
+                  >
+                    <Text
+                      color={
+                        ![
+                          pendingBooking.startDate,
+                          pendingBooking.endDate
+                        ].includes(id)
+                          ? colors.blue
+                          : "white"
+                      }
+                    >
+                      {label}
+                    </Text>
+                  </Box>
+                </Column>
+              ))}
+          </Grid>
+        </Box>
+        <Box paddingVertical="16px">
+          <Box>
+            {rooms.map(room => (
+              <Grid columns={1} gridGaps="16px" width="100%">
+                <Box
+                  padding="32px"
+                  border={`1px solid ${colors.blue}`}
+                  alignItems="center"
+                  justifyContent="space-between"
+                >
+                  <Box>
+                    <Text>{room.name}</Text>
+                  </Box>
+                  <Box alignItems="center" justifyContent="flex-end">
+                    <Text>
+                      Cost $
+                      {room.rate.base *
+                        (pendingBooking.endDate - pendingBooking.startDate)}
+                    </Text>
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
+          </Box>
+        </Box>
+      </Box>
+    </Page>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("react-root"));
--- src/models/store.js
+++ src/models/store.js
@@ -1,42 +1,62 @@
 import PropTypes from "prop-types";
+const now = new Date();
 
-const buildings = [
+const userTable = [
+  { id: 0, role: "Admin", bookings: [] },
+  { id: 1, role: "guest", bookings: [0] }
+];
+
+const bookingInterface = {
+  id: PropTypes.number.isRequired,
+  date: PropTypes.shape(dateInterface),
+  room: PropTypes.shape(roomInterface),
+  guests: PropTypes.arrayOf(userInterface)
+};
+const dateFactory = date => ({
+  label: date.toDateString(),
+  value: date
+});
+const dateTable = [...new Array(20).fill(0)].map((zero, index) => ({
+  id: index,
+  ...dateFactory(addDays(now, index))
+}));
+const roomTable = [
+  {
+    id: 0,
+    name: "Master Bedroom",
+    maxOccupancy: () =>
+      this.beds.reduce(
+        (prev, { maxOccupancy } = curr) => prev + maxOccupancy,
+        0
+      ),
+    beds: [{ id: 0, size: "king", maxOccupancy: "2" }],
+    squareFootage: 800,
+    rate: {
+      base: 100,
+      daily: ({ date }) =>
+        this.holidays.includes(date)
+          ? (1 + holidayInterface.date.dailyIncrease) * this.base
+          : this.base,
+      weekly: ({ days, weeklyDiscount }) =>
+        this.daily * days * (1 - weeklyDiscount),
+      monthly: ({ days, monthlyDiscount }) =>
+        this.daily * days * (1 - monthlyDiscount),
+      currency: "USD"
+    },
+    applicableHolidays: [0, 1],
+    bookedDates: [0, 1, 2],
+    blockedDate: [3, 4]
+  }
+];
+const buildingTable = [
   {
     id: 0,
     name: "Silver Haus",
-    rooms: [
-      {
-        id: 0,
-        name: "Master Bedroom",
-        maxOccupancy: () =>
-          this.beds.reduce(
-            (prev, { maxOccupancy } = curr) => prev + maxOccupancy,
-            0
-          ),
-        beds: [{ id: 0, size: "king", maxOccupancy: "2" }],
-        squareFootage: 800,
-        rate: {
-          base: 100,
-          daily: ({ date }) =>
-            this.holidays.includes(date)
-              ? (1 + holiday.date.dailyIncrease) * this.base
-              : this.base,
-          weekly: ({ days, weeklyDiscount }) =>
-            this.daily * days * (1 - weeklyDiscount),
-          monthly: ({ days, monthlyDiscount }) =>
-            this.daily * days * (1 - monthlyDiscount),
-          currency: "USD"
-        },
-        applicableHolidays: [0, 1],
-        bookedDates: [0, 1, 2],
-        blockedDate: [3, 4]
-      }
-    ]
+    rooms: [0]
   }
 ];
-const now = new Date();
-
-const holidays = [
+const bookingsTable = [{ id: 0, dates: [1, 2], room: 0, guests: [0] }];
+const holidayTable = [
   {
     id: 0,
     name: "Leif Erikson Day",
@@ -44,23 +64,14 @@ const holidays = [
     increase: 0.1
   }
 ];
-const calendar = {
-  dates: [...new Array().fill(0)].map((zero, index) =>
-    dateFactory(addDays(now, index))
-  ),
-  holidays
-};
-const dateFactory = date => ({
-  label: date.toDateString(),
-  value: date
-});
-const date = {
+
+const dateInterface = {
   label: PropTypes.string.isRequired,
   value: PropTypes.Date,
-  holidays: PropTypes.arrayOf(holiday)
+  holidays: PropTypes.arrayOf(holidayInterface)
 };
 
-const building = {
+const buildingInterface = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   maxOccupancy: () =>
@@ -68,30 +79,33 @@ const building = {
       (prev, { maxOccupancy } = curr) => prev + maxOccupancy,
       0
     ),
-  rooms: PropTypes.arrayOf(room).isRequired
+  rooms: PropTypes.arrayOf(roomInterface).isRequired
 };
 
-const room = {
+const roomInterface = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   maxOccupancy: PropTypes.number.isRequired,
-  beds: PropTypes.arrayOf(bed).isRequired,
+  beds: PropTypes.arrayOf(bedInterface).isRequired,
   squareFootage: PropTypes.number.isRequired,
-  rate: PropTypes.shape(rate).isRequired,
-  holidays: PropTypes.arrayOf(holiday).isRequired
+  rate: PropTypes.shape(rateInterface).isRequired,
+  holidays: PropTypes.arrayOf(holidayInterface).isRequired
 };
-const bed = {
+const bedInterface = {
   id: PropTypes.number.isRequired,
   size: PropTypes.oneOf(["king", "queen", "twin"]).isRequired
 };
-const user = {
-  role: PropTypes.oneOf(["Admin", "Member", "Guest"])
+const userInterface = {
+  id: PropTypes.number.isRequired,
+  role: PropTypes.oneOf(["Admin", "Member", "Guest"]),
+  bookings: PropTypes.arrayOf(bookingInterface)
 };
-const rate = { base: PropTypes.number.isRequired };
-const holiday = {
+
+const rateInterface = { base: PropTypes.number.isRequired };
+const holidayInterface = {
   id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
-  date: PropTypes.shape(date).isRequired,
+  date: PropTypes.shape(dateInterface).isRequired,
   increase: PropTypes.number.isRequired
 };
 function addDays(date, days) {
@@ -99,7 +113,13 @@ function addDays(date, days) {
   copy.setDate(date.getDate() + days);
   return copy;
 }
+
+//turns db into fe store
 export const store = {
-  buildings,
-  calendar
+  dates: dateTable,
+  holidays: holidayTable,
+  buildings: buildingTable,
+  bookings: bookingsTable,
+  rooms: roomTable,
+  users: userTable
 };
